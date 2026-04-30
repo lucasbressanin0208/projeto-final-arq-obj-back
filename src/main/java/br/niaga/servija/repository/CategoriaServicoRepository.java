88344813c7c8ecc7be6cd3a513da87ff518c98c1
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-04-30 16:39:02 -0300

    feat(repository for Servico and CategoriaServico)

--- src/main/java/br/niaga/servija/repository/CategoriaServicoRepository.java
+++ src/main/java/br/niaga/servija/repository/CategoriaServicoRepository.java
@@ -1,4 +1,19 @@
 package br.niaga.servija.repository;
 
-public interface CategoriaServicoRepository {
+import br.niaga.servija.models.CategoriaServico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CategoriaServicoRepository extends JpaRepository<CategoriaServico, UUID> {
+
+    Optional<CategoriaServico> findByNomeIgnoreCase(String nome);
+
+    List<CategoriaServico> findAllByAtivaTrue();
+
+    boolean existsByNomeIgnoreCase(String nome);
 }
--- src/main/java/br/niaga/servija/repository/ServiceRepository.java
+++
@@ -1,4 +0,0 @@
-package br.niaga.servija.repository;
-
-public interface ServiceRepository {
-}
