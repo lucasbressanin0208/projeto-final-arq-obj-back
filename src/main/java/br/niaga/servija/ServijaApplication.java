778b32e6e3cb1089b3a59f0ba2e9d2b2af05690a
Lucas Bressanin <lucasb6@al.insper.edu.br>
2026-04-30 14:27:49 -0300

    spring boot application

---
+++ src/main/java/br/niaga/servija/ServijaApplication.java
@@ -0,0 +1,13 @@
+package br.niaga.servija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServijaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServijaApplication.class, args);
+	}
+
+}
