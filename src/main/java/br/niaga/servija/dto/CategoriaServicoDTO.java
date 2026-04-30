5d88d92a09f9e60255e7307e5d4e7a3017306383
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-04-30 16:31:09 -0300

    feat(dto para Servico e CategoriaServico)

--- src/main/java/br/niaga/servija/dto/CategoriaServicoDTO.java
+++ src/main/java/br/niaga/servija/dto/CategoriaServicoDTO.java
@@ -1,32 +1,17 @@
 package br.niaga.servija.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CategoriaServicoDTO {
 
     private String nome;
     private String descricao;
-    private boolean ativo;
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+    private Boolean ativa;
 }
--- src/main/java/br/niaga/servija/dto/ServicoDTO.java
+++ src/main/java/br/niaga/servija/dto/ServicoDTO.java
@@ -1,70 +1,23 @@
 package br.niaga.servija.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ServicoDTO {
 
     private String nome;
     private String descricao;
-    private double preco;
+    private Double preco;
     private Integer duracaoMinutos;
     private UUID categoriaId;
     private UUID prestadorId;
-    private boolean ativo;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-    public Integer getDuracaoMinutos() {
-        return duracaoMinutos;
-    }
-
-    public void setDuracaoMinutos(Integer duracaoMinutos) {
-        this.duracaoMinutos = duracaoMinutos;
-    }
-
-    public UUID getCategoriaId() {
-        return categoriaId;
-    }
-
-    public void setCategoriaId(UUID categoriaId) {
-        this.categoriaId = categoriaId;
-    }
-
-    public UUID getPrestadorId() {
-        return prestadorId;
-    }
-
-    public void setPrestadorId(UUID prestadorId) {
-        this.prestadorId = prestadorId;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+    private Boolean ativo;
 }
