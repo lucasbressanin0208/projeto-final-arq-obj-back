3306e76522bba02344351951b84684e0e2e87c32
GabrielRosa1 <gabrielrosamart@gmail.com>
2026-04-30 15:36:51 -0300

    feat(models Servico e CategoriaServico)

--- src/main/java/br/niaga/servija/models/Servico.java
+++ src/main/java/br/niaga/servija/models/Servico.java
@@ -1,4 +1,80 @@
 package br.niaga.servija.models;
+import java.util.UUID;
 
 public class Servico {
+
+    private UUID id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private Integer duracaoMinutos;
+    private CategoriaServico categoria;
+    private Prestador prestador;
+    private boolean ativo;
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public void setDuracaoMinutos(Integer duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public CategoriaServico getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaServico categoria) {
+        this.categoria = categoria;
+    }
+
+    public Prestador getPrestador() {
+        return prestador;
+    }
+
+    public void setPrestador(Prestador prestador) {
+        this.prestador = prestador;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+
 }
